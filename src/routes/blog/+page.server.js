48b848cd02670197a5fd9b8de8f936a4fa110747
Jesus Calamani <jesuscalamani92@gmail.com>
2023-07-04 21:49:24 -0400

    message

---
+++ src/routes/blog/+page.server.js
@@ -0,0 +1,6 @@
+// @ts-ignore
+export async function load({ fetch }) {
+	const response = await fetch('https://crisapi.vercel.app/api/v3/wear2/query/categories/category/6489005341b831ac5555eea3');
+
+	return await response.json()
+}
