3a25b0c43d259b660d55096d62ea73aaeeeb0435
Jesus Calamani <jesuscalamani92@gmail.com>
2023-07-04 22:38:20 -0400

    message

--- svelte.config.js
+++ svelte.config.js
@@ -9,15 +9,7 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
 		prerender: {
-			handleHttpError: ({ path, referrer, message }) => {
-					// ignore deliberate link to shiny 404 page
-					if (path === '/not-found' && referrer === '/blog/how-we-built-our-404-page') {
-							return;
-					}
-
-					// otherwise fail the build
-					throw new Error(message);
-			}
+			handleHttpError: 'ignore'
 	}
 	},
 	preprocess: vitePreprocess()
