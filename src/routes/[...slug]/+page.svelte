5a45eb78b1088e0811613677109fd040047ef05b
Jesus Calamani <jesuscalamani92@gmail.com>
2023-07-10 09:33:59 -0400

    message

--- src/routes/[...slug]/+page.svelte
+++ src/routes/[...slug]/+page.svelte
@@ -3,13 +3,13 @@
 	import ListProducts from "$lib/components/ListProducts.svelte";
 	export let data;
   // console.log('data', data)
-	const {page, categories, products} = data
+	// const {page, categories, products} = data
 </script>
 
 <svelte:head>
-	<title>{page.data.name}</title>
-	<meta name="description" content={page.data.description} />
-	<meta property="og:image" content={page.data.thumbnailUrl} />
+	<title>{data.page.data.name}</title>
+	<meta name="description" content={data.page.data.description} />
+	<meta property="og:image" content={data.page.data.thumbnailUrl} />
 </svelte:head>
 
 <div
@@ -18,8 +18,8 @@
     <h1
       class="w-10/12 mx-auto md:w-full font-semibold lg:text-4xl text-3xl lg:leading-9 md:leading-7 leading-9 text-center text-cris-inverted"
     >
-      {page.data.name}
+      {data.page.data.name}
     </h1>
   </div>
-<ListCategories {categories}/>
-<ListProducts {products} />
+<ListCategories categories={data.categories}/>
+<ListProducts products={data.products} />
