e3d86158147ac4824865c6e77c6dc81280628400
Jesus Calamani <jesuscalamani92@gmail.com>
2023-07-13 21:43:02 -0400

    message

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,10 +1,9 @@
-import { env } from '$env/dynamic/private';
+
+import { getSite } from '$lib/fetch/sites';
 
 /** @type {import('@sveltejs/kit').Handle} */
 export async function handle({ event, resolve }) {
-  const site = await fetch(
-		`${env.API_URL}/api/${env.API_V}/${env.API_TYPE}/query/sites/${env.API_UID}`
-	).then((response) => response.json());
+  const site = await getSite()
   return resolve(event, {
       transformPageChunk: ({ html }) => html.replace('%theme%', site.data.theme.light),
   });
